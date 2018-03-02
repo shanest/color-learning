bea18ac6419a5f9973971382d25309f491c65892
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2018-03-02 18:38:56 +0100

    removed Point and Space classes; points now just 2-D numpy array

--- partition.py
+++ partition.py
@@ -15,31 +15,6 @@ aRGB = colour.models.ADOBE_WIDE_GAMUT_RGB_COLOURSPACE
 D50 = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D50']
 
 
-class Point(object):
-
-    def __init__(self, value, label=None):
-        self.value = value
-        self.label = label
-
-
-class Space(object):
-
-    def __init__(self, points, dist, zero_point, JND_threshold=0.0):
-        self.points = points
-        self.dist = dist
-        self.zero = zero_point
-        self.JND_threshold = JND_threshold
-
-    def min_dist(self, pt, ls):
-        return min(self.dist(pt.value, p2.value) for p2 in ls)
-
-    def avg_dist(self, p, ls):
-        return sum(self.dist(p.value, p2.value) for p2 in ls) / len(ls)
-
-    def max_dist(self, p, ls):
-        return max(self.dist(p.value, p2.value) for p2 in ls)
-
-
 def RGB_to_Lab(space, pt):
     XYZ = colour.RGB_to_XYZ(pt,
                             space.whitepoint,
@@ -54,50 +29,54 @@ def generate_CIELab_space(rgb_space=aRGB, axis_stride=0.1):
     rgb_points = cartesian(axes)
     lab_points = []
     for row in range(len(rgb_points)):
-        lab_points.append(Point(RGB_to_Lab(rgb_space, rgb_points[row, :])))
+        lab_points.append(RGB_to_Lab(rgb_space, rgb_points[row, :]))
     # dist is squared euclidean, so JND threshold is 0.23^2
-    return Space(lab_points, dist, np.zeros(3), 0.23**2)
+    return np.array(lab_points)
 
 
 class Partition(object):
 
-    def __init__(self, space, labels, temp=0.01, conv=1.0):
+    def __init__(self, points, labels, zero, temp=0.01, conv=1.0):
         self.partition = {label: [] for label in labels}
-        self.centroids = {label: space.zero for label in labels}
+        self.centroids = {label: zero for label in labels}
+        self.labelled_pts = [None]*len(points)
         self.labels = labels
-        self.space = space
+        self.points = points
+        self.zero = zero
         self.temp = temp
         self.conv = conv
         # TODO: write to / read from file?
         self._generate()
 
     def assign_point(self, pt, label):
-        pt.label = label
+        # pt.label = label
+        self.labelled_pts[pt] = label
         # update centroid before .append so that weights are correct
         self.centroids[label] = np.average(
-            [self.centroids[label], pt.value],
+            [self.centroids[label], self.points[pt]],
             axis=0,
             weights=[len(self.partition[label]), 1])
         self.partition[label].append(pt)
 
     def remove_point(self, pt, label):
         # update centroid first
-        self.centroids[label] -= pt.value / len(self.partition[label])
+        self.centroids[label] -= self.points[pt] / len(self.partition[label])
         self.partition[label].remove(pt)
-        pt.label = None
+        self.labelled_pts[pt] = None
+        # pt.label = None
 
     def relabel_point(self, pt, label):
-        self.remove_point(pt, pt.label)
+        self.remove_point(pt, self.labelled_pts[pt])
         self.assign_point(pt, label)
 
     def convexify_region(self, label):
 
         region = self.partition[label]
-        all_points = self.space.points
+        all_points = self.points
         # NOTE: sometimes the iterative calls to this method result in a cell of
         # the partition being empty.  We need to pass over it in those cases.  But
         # maybe we also need better logic to prevent this from happening?
-        if len(region) < len(self.space.zero)+1:
+        if len(region) < len(self.zero)+1:
             return
 
         # TODO: fix two errors in ConvexHull: (i) tuple index out of range; (ii)
@@ -119,14 +98,14 @@ class Partition(object):
         # SOLUTION: add 'noise' to points in space, when making the space, so
         # that it's very unlikely that any will actually be linear!
         convex_hull = scipy.spatial.ConvexHull(
-            [point.value for point in region])
-        misclassified = [point for point in all_points
-                         if point_in_hull(point.value, convex_hull)
+            [all_points[point] for point in region])
+        misclassified = [point for point in range(len(all_points))
+                         if point_in_hull(all_points[point], convex_hull)
                          and point not in region]
         if len(misclassified) > 0:
             num_to_move = int(self.conv*len(misclassified))
             misclassified.sort(
-                key=lambda point: self.space.min_dist(point, region))
+                key=lambda point: min_dist(point, region))
             to_move = misclassified[:num_to_move]
             for point in to_move:
                 self.relabel_point(point, label)
@@ -134,13 +113,13 @@ class Partition(object):
     def degree_of_convexity_of_cell(self, label):
         # empty regions have "degree" 1.0
         region = self.partition[label]
-        if len(region) < len(self.space.zero)+1:
+        if len(region) < len(self.zero)+1:
             return 1.0
 
         convex_hull = scipy.spatial.ConvexHull(
-            [point.value for point in region])
-        num_inside_hull = sum(int(point_in_hull(pt.value, convex_hull))
-                              for pt in self.space.points)
+            [self.points[point] for point in region])
+        num_inside_hull = sum(int(point_in_hull(pt, convex_hull))
+                              for pt in self.points)
         return len(region) / num_inside_hull
 
     def degree_of_convexity(self):
@@ -150,18 +129,19 @@ class Partition(object):
             # should the weights be different -- uniform? -- for this mean?
             weights=[len(partition[label]) for label in partition])
 
-    def _generate(self):
+    def _generate(self, init_n=5):
 
-        points = self.space.points
+        points = self.points
         unlabeled = range(len(points))
-        min_dist = self.space.min_dist
+        # min_dist = self.space.min_dist
         labels = self.labels
 
         # initialize with one seed point for each label
         seeds = random.sample(unlabeled, len(labels))
         for label in labels:
             unlabeled.remove(seeds[label])
-            self.assign_point(points[seeds[label]], label)
+            #self.assign_point(points[seeds[label]], label)
+            self.assign_point(seeds[label], label)
 
         while len(unlabeled) > 0:
             # get random point
@@ -171,19 +151,16 @@ class Partition(object):
             # choose cell based on how close it is to the other cells
             dists = [min_dist(to_add, self.partition[label])
                      for label in labels]
-            below_JND = np.where(dists < self.space.JND_threshold)[0]
-            if len(below_JND) > 0:
-                print 'below JND'
-                cell = labels[np.random.choice(below_JND)]
-            else:
-                # TODO: parameterize the f in f(min_dist(pt, label))?
-                norm_dists = dists / max(dists)
-                weights = -np.array(norm_dists)
-                probs = softmax(weights, self.temp)
-                cell = np.random.choice(labels, p=probs)
+
+            # TODO: parameterize the f in f(min_dist(pt, label))?
+            norm_dists = dists / max(dists)
+            weights = -np.array(norm_dists)
+            probs = softmax(weights, self.temp)
+            cell = np.random.choice(labels, p=probs)
 
             # add both to partition and labels array
-            self.assign_point(to_add, cell)
+            #self.assign_point(to_add, cell)
+            self.assign_point(new_idx, cell)
             # mark as labeled
             unlabeled.remove(new_idx)
 
@@ -205,9 +182,9 @@ class Partition(object):
 
     def _get_mislabeled(self):
 
-        points = self.space.points
+        points = self.points
         mislabeled = set()
-        for label in labels:
+        for label in self.labels:
             convex_hull = scipy.spatial.ConvexHull([point.value for point in
                                                     self.partition[label]])
             not_in_hull = set([point for point in points if not
@@ -220,6 +197,10 @@ class Partition(object):
 def dist(p1, p2):
     """Distance of n-d coordinates. """
     return np.sum((p1 - p2)**2)
+
+
+def min_dist(pt, ls):
+    return min(dist(pt, p2) for p2 in ls)
 
 
 def width(ls):
@@ -255,10 +236,12 @@ def distance_to_convex_hull(point, hull):
 
 def partition_to_img(partition, axes):
     img = np.zeros([axes[0][1], axes[1][1]])
-    for label in partition:
-        for point in partition[label]:
+    part = partition.partition
+    points = partition.points
+    for label in part:
+        for point in part[label]:
             # de-noise point value
-            pt = np.around(point.value).astype(int)
+            pt = np.around(points[point]).astype(int)
             img[pt[0], pt[1]] = label
     return img
 
@@ -288,7 +271,7 @@ def generate_2D_grid(temps, convs, axis_length):
     # add noise so that co-linear points are very unlikely
     noise = np.random.random(points.shape) * 1e-5
     points += noise
-    points = [Point(points[row, :]) for row in range(len(points))]
+    # points = [Point(points[row, :]) for row in range(len(points))]
     labels = range(7)
 
     fig, axes = plt.subplots(nrows=len(temps), ncols=len(convs))
@@ -298,10 +281,9 @@ def generate_2D_grid(temps, convs, axis_length):
             temp = temps[row]
             conv = convs[col]
             print '{}, {}'.format(temp, conv)
-            space = Space(points, dist, np.zeros(2))
-            partition = Partition(space, labels, temp, conv)
+            partition = Partition(points, labels, np.zeros(2), temp, conv)
             print partition.degree_of_convexity()
-            img = partition_to_img(partition.partition, my_axes)
+            img = partition_to_img(partition, my_axes)
             ax = axes[row, col]
             ax.set_yticks([])
             ax.set_xticks([])
@@ -319,7 +301,8 @@ def generate_2D_grid(temps, convs, axis_length):
 
 if __name__ == '__main__':
 
-    generate_2D_grid([1, 0.1, 0.01, 0.001, 0.0005], [0, 0.25, 0.5, 0.75, 1.0], 40)
+    generate_2D_grid([1, 0.1, 0.01, 0.001, 0.0005], [0, 0.25, 0.5, 0.75, 1.0],
+                    40)
 
     """
     space = generate_CIELab_space(axis_stride=0.1)
